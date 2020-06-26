6a8a45edd05d475b58d5f3b3d0698bead6dc85fc
matus dubrava <matusdubrava@gmail.com>
2020-06-26 12:31:01 +0200

    finished view component

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,11 +1,29 @@
-import { User } from './models/User';
-import { UserForm } from './views/UserForm';
+// import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
-const root = document.querySelector('#root');
-if (root) {
-    const userForm = new UserForm(root, user);
-    userForm.render();
-} else {
-    throw new Error('root element not found');
-}
+// const user = User.buildUser({ name: 'NAME', age: 20 });
+// const root = document.querySelector('#root');
+// if (root) {
+//     const userEdit = new UserEdit(root, user);
+//     userEdit.render();
+//     console.log(userEdit);
+// } else {
+//     throw new Error('root element not found');
+// }
+
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) =>
+    User.buildUser(json)
+);
+
+users.on('change', () => {
+    const root = document.getElementById('root');
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
+
+users.fetch();
